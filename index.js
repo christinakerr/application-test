41d743d585bbaa1d055d4ed1ea8d482a06b9d5e1
Christi Kerr <christinakerr394@gmail.com>
2021-05-13 16:52:35 -0500

    Time's up

--- index.js
+++ index.js
@@ -11,11 +11,10 @@ function calculator() {
         let equation = response.equation;
 
 
-        for (let i = 0; i < equation.length; i++) {
-
-            let calculations = [];
+        for (let i = 0; i < equation.length; i++) { // Handle parentheses
 
             if (equation[i] === "(") {
+                let calculations = [];
                 for (let j = 1; j < equation.length; j++) {
                     if (equation[j] === ")") {
                         calculations.push({ "operation": equation.slice(i + 1, j), "index": i })
@@ -36,27 +35,52 @@ function calculator() {
                     const result = singleOperation(calc.operation.slice(0, operator.index), operator.op, calc.operation.slice(operator.index + 1))
                     equation = [equation.slice(0, calc.index), result, equation.slice(calc.index)].join("");
                 }
-                
-
             }
         }
-        if (equation.length === 1){
+
+        if (equation.length === 1) {
             return equation
         }
 
-        
+
+        handleOperators("*");
+        handleOperators("/");
+        handleOperators("+");
+        handleOperators("-")
 
 
-        // console.log(equation);
-        // return equation;
+        function handleOperators(op) {
+
+            let indices = operatorSearch(equation, op); // Handle multiplication
+
+            let i = 0;
+            while (indices.length > 0) {
+                let startEnd = calculationSearch(equation, indices[i])
+                let num1 = equation[startEnd[0]];
+                let num2 = equation[startEnd[1]];
+                if (startEnd[2] > 1) {
+                    num1 = equation.slice(startEnd[0], startEnd[2])
+                }
+                if (startEnd[3] > 1) {
+                    num2 = equation.slice(startEnd[1], startEnd[3])
+                }
+
+                const result = singleOperation(num1, op, num2)
+                equation = equation.replace(equation.slice(startEnd[0], startEnd[1] + 1), result)
+                indices = operatorSearch(equation, op);
+            }
+        }
+
+        return equation;
     }).catch(err => console.log(err))
 }
+
 
 function singleOperation(number1, operator, number2) {
 
     num1 = parseInt(number1);
     num2 = parseInt(number2);
-    
+
     switch (operator) {
         case "+":
             return num1 + num2;
@@ -79,6 +103,36 @@ function operatorSearch(eq, operator) {
         }
     }
     return indexes;
+}
+
+function calculationSearch(eq, index) {
+    let start;
+    let end;
+    let startDigits = 0;
+    let endDigits = 0
+    if (eq.length === 3) {
+        return [0, 2, 1, 1];
+    }
+
+    for (let i = index + 1; i < eq.length; i++) {
+        if (!parseInt(eq[i])) {
+            end = i - 1;
+            break;
+        } else {
+            endDigits++;
+        }
+    }
+
+    for (let i = index - 1; i >= -1; i--) {
+        
+        if (!parseInt(eq[i]) || i === -1) {
+            start = i + 1;
+            break;
+        } else {
+            startDigits++
+        }
+    }
+    return [start, end, startDigits, endDigits]
 }
 
 
